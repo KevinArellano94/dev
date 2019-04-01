1ba8fef7e4fc7997dc03c08556f96cc2b30ace2a
Kevin Arellano <Kevin.Arellano94@gmail.com>
2019-04-01 14:56:50 -0400

    .
    .

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,25 +2,7 @@ import React, { Component } from "react";
 
 class Contact extends React.Component {
   render() {
-    return [
-      <div class="container">
-        <form name="contact" netlify>
-          <p>
-            <label>
-              Name <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Email <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </div>
-    ];
+    return [<div class="container" />];
   }
 }
 

b74083a6fffdd20b172ecc402a63cb6aeeb6ce7a
Kevin Arellano <Kevin.Arellano94@gmail.com>
2019-04-01 15:03:14 -0400

    .
    .

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 class Contact extends React.Component {
   render() {
-    return [<div class="container" />];
+    return [
+      <div class="container">
+        <object
+          type="text/html"
+          data="https://t-rex-scans.netlify.com/contact.html"
+        />
+      </div>
+    ];
   }
 }
 

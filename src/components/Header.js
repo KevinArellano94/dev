155193691fd8c2173db00f8db14e7b4f8cfba35b
Kevin Arellano <Kevin.Arellano94@gmail.com>
2019-04-02 14:11:44 -0400

    .
    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import logo from "../images/home_World_Map.jpg";
+import logo_image from "../images/home_World_Map.jpg";
+import NetlifyAuth from "../components/NetlifyAuth.js";
 
 class Header extends React.Component {
   render() {
@@ -39,9 +40,20 @@ class Header extends React.Component {
             </li>
           </ul>
         </nav>
-        <div data-netlify-identity-menu />
+        <logo scrolled={"scrolled"} navigationStyle={"navigationStyle"} />
+        <ul className="nav navbar-nav">
+          <li>
+            <NetlifyAuth />
+          </li>
+        </ul>
+        <searchIcon toggleSearch={"toggleSearch"} />
+        <ul className="nav navbar-nav">
+          <li>
+            <div data-netlify-identity-menu />
+          </li>
+        </ul>
         <center>
-          <img src={logo} class="rounded" width="100%" alt="Logo" />
+          <img src={logo_image} class="rounded" width="100%" alt="Logo" />
         </center>
       </div>
     ];
--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import netlifyIdentity from "netlify-identity-widget";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import Contact from "./components/Contact.js";
+window.netlifyIdentity = netlifyIdentity;
+netlifyIdentity.init();
 
 ReactDOM.render(
   <ConcurrentMode>

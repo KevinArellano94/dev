4311e6440832f76efe9aae44b366226d891b1519
Kevin Arellano <Kevin.Arellano94@gmail.com>
2019-04-02 15:47:14 -0400

    .
    .

--- src/components/NetlifyAuth.js
+++ src/components/NetlifyAuth.js
@@ -39,7 +39,7 @@ class NetlifyAuth extends React.Component {
     }
     return (
       <a href="#" onClick={this.handleLogOutClick}>
-        Log out of {user.name} | {user.email}
+        Log out of {user.full_name} | {user.email} | {user.roles}}
       </a>
     );
   }
